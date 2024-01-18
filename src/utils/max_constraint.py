62189abfa85796925f217945616caa04371e505c
marnugue <marcostorterra@gmail.com>
2024-01-18 15:09:04 +0100

    Epsilon constraint method for multiobjective optimization

---
+++ src/utils/max_constraint.py
@@ -0,0 +1,48 @@
+from pyomo.environ import Constraint, Model, Var, Binary, Reals
+from typing import Any
+
+
+def max_constraints(model: Model, var1: str | Any, var2: str | Any, **kwargs):
+    BIG_M = 10e6
+    
+    if kwargs:
+        keys = list(kwargs.keys())
+        x = var1
+        y = var2
+        var1_name = kwargs[keys[0]]
+        var2_name = kwargs[keys[1]]
+    else:
+        x = getattr(model, var1)
+        y = getattr(model, var2)
+        var1_name = var1
+        var2_name = var2
+        
+    var_name_aux = f"max_var_aux_{var1_name}_{var2_name}"
+    max_value_name = f"max_value_{var1_name}_{var2_name}"
+    max_value_name_alternative = f"max_value_{var2_name}_{var1_name}"
+
+    if not hasattr(model, max_value_name) and \
+       not hasattr(model, max_value_name_alternative):
+        var_aux = Var(range(2), domain=Binary)
+
+        max_value = Var(domain=Reals)
+
+        setattr(model, var_name_aux, var_aux)
+
+        setattr(model, max_value_name, max_value)
+
+        c1 = Constraint(expr=x <= max_value)
+        c2 = Constraint(expr=y <= max_value)
+        c3 = Constraint(expr=x >= BIG_M * (1 - getattr(model, var_name_aux)[0]))
+        c4 = Constraint(expr=y >= BIG_M * (1 - getattr(model, var_name_aux)[1]))
+        c5 = Constraint(
+            expr=sum(getattr(model, var_name_aux)[i] for i in range(2)) >= 1
+        )
+        setattr(model, f"c1_{max_value_name}", c1)
+        setattr(model, f"c2_{max_value_name}", c2)
+        setattr(model, f"c3_{max_value_name}", c3)
+        setattr(model, f"c4_{max_value_name}", c4)
+        setattr(model, f"c5_{max_value_name}", c5)
+
+    return max_value_name if hasattr(model, max_value_name) \
+        else max_value_name_alternative
--- test/test_epsilon_constraint.py
+++ test/test_epsilon_constraint.py
@@ -1,6 +1,7 @@
 import unittest
 from pyomo.environ import *
-from src.utils.epsilon_constraint import EpsilonConstraintAugment
+from src.utils.epsilon_constraint import EpsilonConstraintAugment, Solution
+
 
 class TestEpsilonConstraint(unittest.TestCase):
     
@@ -14,10 +15,23 @@ class TestEpsilonConstraint(unittest.TestCase):
         model.C2 = Constraint(expr = model.X2 <= 40)
         model.C3 = Constraint(expr = 5 * model.X1 + 4 * model.X2 <= 200)
 
-        model.O_f1 = Objective(expr= model.X1  , sense=maximize)
+        model.O_f1 = Objective(expr= model.X1 , sense=maximize)
         model.O_f2 = Objective(expr= 3 * model.X1 + 4 * model.X2 , sense=maximize)
         eps = EpsilonConstraintAugment(model)
         solutions = eps.execute()
         efficient_solutions = eps.get_efficient_solutions(solutions)
+        
         print('')
-
+    
+class TestEfficientSolutionFilter(unittest.TestCase):
+    def test_that_if_there_are_3_solution_and_one_of_them_is_nondominant_then_return_2_remaining_solutions(self):
+        
+        solutions = [Solution(_obj_values={'a':2, 'b':3}),
+                            Solution(_obj_values={'a':3, 'b':2}),
+                            Solution(_obj_values={'a':2, 'b':2})]
+        expected_solutions = solutions[:2]
+        
+        eps = EpsilonConstraintAugment()
+        returned_solutions = eps.get_efficient_solutions(solutions)
+        
+        self.assertListEqual(expected_solutions, returned_solutions)
